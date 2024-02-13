7554c9646f9032a7f00bdcd5de76bcd067291124
Maciej Necka <macieknecka@gmail.com>
2024-02-13 16:40:47 +0100

    Db.json with movies added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import ResetStyles from './styles/css/Reset';
 import GlobalStyles from './styles/css/Global';
-import GlobalFonts from './styles/css/fonts/GlobalFonts';
+import GlobalFonts from './styles/fonts/GlobalFonts';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/css/theme';
 
